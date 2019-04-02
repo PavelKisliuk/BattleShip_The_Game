edfd3fbc2998d30e9967f62294c58d56c04a6f17
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-04-02 21:15:06 +0300

    Change Area int to CellsType

--- src/com/github/PavelKisliuk/module/data/area/Area.java
+++ src/com/github/PavelKisliuk/module/data/area/Area.java
@@ -3,7 +3,7 @@ package com.github.PavelKisliuk.module.data.area;
 import com.github.PavelKisliuk.module.data.ships.Ship;
 
 public class Area extends AbstractArea {
-	private int[][] area;
+	private CellsType[][] area;
 	private Ship[] ships;
 
 	public Area()
