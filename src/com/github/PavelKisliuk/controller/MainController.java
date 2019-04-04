92116334572b9570098922d41dd45b784de8c0ce
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-04-04 14:19:35 +0300

    add MainController

---
+++ src/com/github/PavelKisliuk/controller/MainController.java
@@ -0,0 +1,46 @@
+package com.github.PavelKisliuk.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+
+public class MainController {
+
+	@FXML
+	private GridPane playerGridPane;
+
+	@FXML
+	private GridPane opponentGridPane;
+
+	@FXML
+	private Button startButton;
+
+	@FXML
+	private CheckBox goFirstCheckBox;
+
+	@FXML
+	private Button newGameButton;
+
+	@FXML
+	void imageViewsOnMouseClicked(MouseEvent event) {
+
+	}
+
+	@FXML
+	void imageViewsOnMouseEntered(MouseEvent event) {
+
+	}
+
+	@FXML
+	void imageViewsOnMouseExited(MouseEvent event) {
+
+	}
+
+	@FXML
+	void initialize() {
+
+	}
+
+}
