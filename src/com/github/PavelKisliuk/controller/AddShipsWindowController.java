376bd58f40e686a64fc6ce4c5e9685dec9675770
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-04-09 16:58:38 +0300

    AddShipsWindowController skeleton

--- src/com/github/PavelKisliuk/controller/AddShipsWindowController.java
+++ src/com/github/PavelKisliuk/controller/AddShipsWindowController.java
@@ -1,4 +1,47 @@
 package com.github.PavelKisliuk.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+
 public class AddShipsWindowController {
+
+	@FXML
+	private Button addShipButton;
+
+	@FXML
+	private Button shipAddedButton;
+
+	@FXML
+	private GridPane playerGridPane;
+
+	@FXML
+	private Label infoLabel;
+
+	@FXML
+	private Button discardButton;
+
+	@FXML
+	private Button okButton;
+
+	@FXML
+	private Button cancelButton;
+
+	@FXML
+	void imageViewsOnMouseClicked(MouseEvent event) {
+
+	}
+
+	@FXML
+	void imageViewsOnMouseEntered(MouseEvent event) {
+
+	}
+
+	@FXML
+	void imageViewsOnMouseExited(MouseEvent event) {
+
+	}
+
 }
